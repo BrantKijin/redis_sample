214f6b0d06d65b19d9c0f44612f6dd61cb3809ed
BrantKijin <upper0house@gmail.com>
2024-02-15 20:09:09 +0900

    feat: init

--- src/main/java/com/fastcampus/cache/service/UserService.java
+++ src/main/java/com/fastcampus/cache/service/UserService.java
@@ -12,6 +12,7 @@ import com.fastcampus.cache.domain.repository.RedisHashUserRepository;
 import com.fastcampus.cache.domain.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
+
 //import static com.example.cache.config.CacheConfig.CACHE1;
 @Service
 @RequiredArgsConstructor
@@ -22,34 +23,33 @@ public class UserService {
 	private final RedisTemplate<String, User> userRedisTemplate;
 	private final RedisTemplate<String, Object> objectRedisTemplate;
 
-	public User getUser(final Long id){
+	public User getUser(final Long id) {
 		var key = "users:%d".formatted(id);
-		var cachedUser =objectRedisTemplate.opsForValue().get(key);
-		if(cachedUser != null){
-			return (User) cachedUser;
+		var cachedUser = objectRedisTemplate.opsForValue().get(key);
+		if (cachedUser != null) {
+			return (User)cachedUser;
 		}
-		User user= userRepository.findById(id).orElseThrow();
+		User user = userRepository.findById(id).orElseThrow();
 		objectRedisTemplate.opsForValue().set(key, user, Duration.ofSeconds(30));
 		return user;
 	}
 
-	public RedisHashUser getUser2(final Long id){
-		return redisHashUserRepository.findById(id).orElseGet(() ->{
+	public RedisHashUser getUser2(final Long id) {
+		return redisHashUserRepository.findById(id).orElseGet(() -> {
 			User user = userRepository.findById(id).orElseThrow();
 			return redisHashUserRepository.save(RedisHashUser.builder()
-					.id(user.getId())
-					.name(user.getName())
-					.email(user.getEmail())
-					.createdAt(user.getCreatedAt())
-					.updatedAt(user.getUpdatedAt())
+				.id(user.getId())
+				.name(user.getName())
+				.email(user.getEmail())
+				.createdAt(user.getCreatedAt())
+				.updatedAt(user.getUpdatedAt())
 				.build());
 		});
 	}
 
 	//@Cacheable(cacheNames = CACHE1, key = "'user:' + #id")
-	public User getUser3(final Long id){
+	public User getUser3(final Long id) {
 		return userRepository.findById(id).orElseThrow();
 	}
-
 
 }
